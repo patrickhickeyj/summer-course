82e02648702c76f71368d9dfcae1aab46328f57a
patrickhickeyj <patrick.hickey.j@gmail.com>
2026-08-04 13:45:38 -0400

    prob 2 take 3

--- Python/Weekly Problem Sets/Problem Set 4.py
+++ Python/Weekly Problem Sets/Problem Set 4.py
@@ -105,8 +105,8 @@ class Pantry():
     def __init__(self, ingredients):
         self.ingredients = set(ingredients)
 
-    def get_items(self, ing):
-        return(set(ing))
+    def get_items(self):
+        return(set(self.ingredients))
 
     def add_ingredients(self, extra_ingredients):
         self.ingredients = set(extra_ingredients).union(self.ingredients)
