2ea87e89e8d9c955bebc02126f703f131669825a
patrickhickeyj <patrick.hickey.j@gmail.com>
2026-08-07 13:26:57 -0400

    prob 3

--- Python/Weekly Problem Sets/Problem Set 5.py
+++ Python/Weekly Problem Sets/Problem Set 5.py
@@ -1,4 +1,7 @@
-#Problem 1
+import requests
+
+
+# Problem 1
 def recursive_squares(n):
     if type(n) != int:
         return []
@@ -7,7 +10,8 @@ def recursive_squares(n):
     if n == 1:
         return [1]
     else:
-        return recursive_squares(n-1) + [n**2]
+        return recursive_squares(n - 1) + [n**2]
+
 
 def palindrome_checker(word):
     cleaned = word.lower()
@@ -18,10 +22,12 @@ def palindrome_checker(word):
     else:
         return palindrome_checker(cleaned[1:-1])
 
+
 def length(tlist):
     if tlist:
         return length(tlist[:-1]) + 1
     return 0
+
 
 def flatten(mixed_list):
     if len(mixed_list) < 1:
@@ -31,7 +37,9 @@ def flatten(mixed_list):
     else:
         return flatten(mixed_list[0]) + flatten(mixed_list[1:])
 
-#Problem 2
+
+# Problem 2
+
 
 def fibonacci(n):
     if n == 0:
@@ -39,7 +47,8 @@ def fibonacci(n):
     elif n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 def count_ways(n):
     if n == 0:
@@ -47,13 +56,15 @@ def count_ways(n):
     if n == 1:
         return 1
     else:
-        return count_ways(n-2) + count_ways(n-1)
+        return count_ways(n - 2) + count_ways(n - 1)
+
 
 def grid_paths(m, n):
-    if m == 1 or n ==1:
+    if m == 1 or n == 1:
         return 1
     else:
-        return grid_paths(m, n-1) + grid_paths(m-1, n)
+        return grid_paths(m, n - 1) + grid_paths(m - 1, n)
+
 
 def permutation(perm_list):
     if not perm_list:
@@ -68,11 +79,74 @@ def permutation(perm_list):
         return holder
 
 
-if __name__ == '__main__':
+# Problem 3
+base = "https://jsonplaceholder.typicode.com"
+chal_base = "https://reqres.in/api/users"
+
+
+def get_user(user_id: int) -> dict:
+    bridge = f"/users/{user_id}"
+    holder = requests.get(base + bridge)
+    if holder.status_code != 200:
+        print(holder.status_code)
+        return {}
+    else:
+        return holder.json()
+
+
+def create_user(user_dict):
+    bridge = "/users"
+    create_holder = requests.post(base + bridge, user_dict)
+    if create_holder.status_code != 201:
+        print(create_holder.status_code)
+        return {}
+    else:
+        return create_holder.json()
+
+
+def update_user(user_id, name, job):
+    bridge = f"/users/{user_id}"
+    submit_dict = {"name": name, "job": job}
+    res_dict = requests.put(base + bridge, json=submit_dict)
+    if res_dict.status_code != 200:
+        print(res_dict.status_code)
+        return {}
+    else:
+        return res_dict.json()
+
+
+def delete_user(user_id):
+    bridge = f"/users/{user_id}"
+    del_dict = requests.delete(base + bridge)
+    return del_dict.status_code == 200
+
+
+def get_users_page(page):
+    bridge = f"?page={page}"
+    header = {"x-api-key": "free_user_3Hb3S2tmGt30vw7rrZkQcl8go3q"}
+    chal_page = requests.get(chal_base + bridge, headers=header)
+    if chal_page.status_code != 200:
+        return []
+    else:
+        return chal_page.json()["data"]
+
+
+def partial_update_user(user_id, updates):
+    bridge = f"/{user_id}"
+    header = {"x-api-key": "free_user_3Hb3S2tmGt30vw7rrZkQcl8go3q"}
+    chal_update = requests.patch(chal_base + bridge, headers=header, json=updates)
+    if chal_update.status_code != 200:
+        return {}
+    else:
+        return chal_update.json()
+
+
+if __name__ == "__main__":
+
     def prob1():
         print(recursive_squares(5))
         print(recursive_squares([]))
-        print(palindrome_checker('aab'))
+        print(palindrome_checker("aab"))
         print(length([5, 6, 7, 8]))
         print(flatten([1, [2, 3], [4], 5]))
 
@@ -81,5 +155,16 @@ if __name__ == '__main__':
         print(count_ways(4))
         print(grid_paths(3, 3))
         print(permutation([1, 2, 3]))
-    prob1()
-    prob2()
+
+    def prob3():
+        print(get_user(4))
+        example_user = {"name": "pat", "job": "army"}
+        print(create_user(example_user))
+        print(update_user(3, "pat", "job"))
+        print(delete_user(3))
+        print(get_users_page(2))
+        print(partial_update_user(2, {"job": "Senior Developer"}))
+
+    # prob1()
+    # prob2()
+    prob3()
